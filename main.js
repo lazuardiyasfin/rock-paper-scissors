a5ef8854e5dbb4605506b33a40dc5ff4996cd8de
lazuardiyasfin <70686532+lazuardiyasfin@users.noreply.github.com>
2025-02-01 21:15:13 +0700

    Add the logic to get the computer choice

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    return randomNumber == 0? "rock" :
+        randomNumber == 1? "paper" :
+        "scissors";
+}
