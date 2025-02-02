dcfb255c8a30014a18bb0cbb8f30f0aa3041c4ac
lazuardiyasfin <70686532+lazuardiyasfin@users.noreply.github.com>
2025-02-02 08:39:34 +0700

    Add logic to play the entire game

--- main.js
+++ main.js
@@ -18,31 +18,36 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
-    computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
-
-    if (humanChoice == computerChoice) {
-        console.log(`It's tie! Both players throws ${humanChoice}`);
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+        computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+    
+        if (humanChoice == computerChoice) {
+            console.log(`It's tie! Both players throws ${humanChoice}`);
+        }
+        else if ((humanChoice == "rock" && computerChoice == "scissors") ||
+            (humanChoice == "scissors" && computerChoice == "paper") ||
+            (humanChoice == "paper" && computerChoice == "rock")) {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        }
+        else {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        }
+    
+        return;
     }
-    else if ((humanChoice == "rock" && computerChoice == "scissors") ||
-        (humanChoice == "scissors" && computerChoice == "paper") ||
-        (humanChoice == "paper" && computerChoice == "rock")) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScore++;
-    }
-    else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        computerScore++;
+
+    for (let round = 1; round <= 5; round++) {
+        playRound(getHumanChoice(), getComputerChoice());
     }
 
     return;
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
