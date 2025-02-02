9af9df3c92af236990d34dc4292b19c9babd864b
lazuardiyasfin <70686532+lazuardiyasfin@users.noreply.github.com>
2025-02-02 08:29:37 +0700

    Add the logic to play a single round

--- main.js
+++ main.js
@@ -21,3 +21,28 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+    computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+
+    if (humanChoice == computerChoice) {
+        console.log(`It's tie! Both players throws ${humanChoice}`);
+    }
+    else if ((humanChoice == "rock" && computerChoice == "scissors") ||
+        (humanChoice == "scissors" && computerChoice == "paper") ||
+        (humanChoice == "paper" && computerChoice == "rock")) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    }
+    else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+
+    return;
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
